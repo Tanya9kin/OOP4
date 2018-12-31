afa7cec3be1421f7d5b011df908b4aeaa207b1a2
Tanya Devetykin <tanya9kin@gmail.com>
2018-12-31 18:12:26 +0200

    part 2 done and started logic for part 3

--- src/OOP/Solution/OOPResultImpl.java
+++ src/OOP/Solution/OOPResultImpl.java
@@ -11,33 +11,7 @@ public class OOPResultImpl implements OOPResult {
         result_type = new_result;
         message = new_message;
     }
-    /*
-    for later.....implementation:
-    get the expected exceptions of the test class and the message for it
-    check that it was
-    SUCCESS -
-            * there was no exception thrown or
-            * if the exception that was thrown is expected and has the message
-            that we expected in OOPExpectedException in the tests class
-            *a test method is expecting an exception if it has a non static field of the type OOPExpectedException
-            and has something other then null to return. (expect() method was done on it)
-            * the expect() method needs to be invoked IN THE TEST  - very important
-            * return null in this case
 
-    if an unexpected exception was thrown
-    FAILURE -
-            *if it was OOPAssertionFailure
-    EXCPECTED_EXCEPTION_MISMATCH -
-            *if it wasn't OOPAssertionFailure
-            * AND
-            * it was an unexpected exception
-            * OR  expected exception but the message was off (didn't exist or was wrong)
-            * return OOPExceptionMismatch.getMessage()
-    ERROR -
-            *an exception was thrown that we did not expect
-            * or no exception was thrown although we expected it
-            * return the class of the exception (we expected or did not expect)
-     */
     @Override
     public OOPTestResult getResultType() { return result_type; }
 
@@ -56,5 +30,10 @@ public class OOPResultImpl implements OOPResult {
         if(real_obj.getResultType() == result_type && real_obj.getMessage().equals(message)){
             return true;
         }
+        return false;
     };
+
+    //TO DO:
+    // need to write hash
+    //need to check if in equals we need to call the equals of super
 }
--- src/OOP/Solution/OOPUnitCore.java
+++ src/OOP/Solution/OOPUnitCore.java
@@ -43,9 +43,16 @@ public class OOPUnitCore {
             * (probably in another function for readability)
             * make a collection for all the methods with annotation OOPTest
             * map : for each method make collection of the OOPBefore
+            *       these methods are taken from all the tests for me and my daddys
             *       ordered by inheritance from father to son
-            *   these methods are taken from all the tests for me and my daddys
-            * map : for each method make collection of the OOPAfter (unordered) same way
+            *       LOGIC:
+            *           start with me and go up the inheritance -
+            *           get all the OOPBefore which have the methods name in their list
+            *           add them to this list
+            *           if you see another method with the same name, don't put it in
+            *           reverse list
+            * map : for each method make collection of the OOPAfter same way only son to father order
+            *
             * filter tests by tag (if tagFlag is True)
             * order tests by order if OOPTestClassType is ORDERED
             *       if some daddy is UNORDERED - order for them is 0 (default value)
@@ -64,21 +71,73 @@ public class OOPUnitCore {
             *       if a method throws an exception
             *           restore fields
             *           add OOPTestResult.ERROR for this test method
+            *               the message will be the name of the Exception class
             *           on to the next test
-            *   run method:
-            *
+            *   run method in try catch block: (some reference shit here:)
+            SUCCESS -
+                * there was no exception thrown or
+                * if the exception that was thrown is expected and has the message
+                    that we expected in OOPExpectedException in the tests class
+                * a test method is expecting an exception if it has a non static field of the type OOPExpectedException
+                    and has something other then null to return. (expect() method was done on it)
+                * the expect() method needs to be invoked IN THE TEST  - very important
+                * return null in this case
+
+            if an unexpected exception was thrown:
+            FAILURE -
+                *if it was OOPAssertionFailure
+            EXPECTED_EXCEPTION_MISMATCH -
+                *if it wasn't OOPAssertionFailure
+                * AND
+                * it was an unexpected exception
+                * OR  expected exception but the message was off (didn't exist or was wrong)
+                * return OOPExceptionMismatch.getMessage()
+            ERROR -
+                *an exception was thrown that we did not expect
+                * or no exception was thrown although we expected it
+                * return the class of the exception (we expected or did not expect)
+
+            AND THAT IS WHY LOGIC:
+                * if there is a field with annotation @OOPExceptionRule save it in variable
+                * check that it is non static and has a return value for expect other than null
+                * #3Q: can this be an exception of another class? I mean - not OOPExpectedException?
+                * A:
+                * #4Q: The expect method has to be invoked in the test - does it mean in each
+                *   test method? if it is - how do we check this?
+                * A:
+                *
+                * catch (expected exception)
+                *   if the message is the expected message all good - SUCCESS
+                *       message is:
+                *   else - EXPECTED_EXCEPTION_MISMATCH
+                *       message is: OOPExceptionMismatch.getMessage()
+                * catch (OOPAssertionFailure)
+                *   FAILURE
+                *   message is:
+                * catch (...)
+                *   ERROR
+                *   message is the class of the caught exception
+                * if no exception but we expected one:
+                *   ERROR
+                *   message is the class of the expected exception
+                *
             *   run OOPAfter:
             *       Backup fields
             *       run method in try catch
             *       if a method throws an exception
             *           restore fields
             *           add OOPTestResult.ERROR for this test method
+            *               the message will be the name of the Exception class
             *           move on to the next test
             *
             *
             *  Add an entry to the summery with the name of the method and result
             *
      */
+
+    static getAnnotated(){
+
+    }
 
     //maybe write a function that gets all of the methods with annotation "***"
     //if we need it ordered we can make it so on demand
